9b7268ffee645ac6ad94636b2256f0101f643de7
Akshay23504 <aaravind@syr.edu>
2018-11-26 10:09:43 -0500

    Pfff....Too many errors...Fixed em all

--- src/app/customer-dashboard/customer-dashboard.component.ts
+++ src/app/customer-dashboard/customer-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {} from '@types/googlemaps';
+import LatLng = google.maps.LatLng;
 
 @Component({
   selector: 'app-customer-dashboard',
@@ -9,55 +10,57 @@ import {} from '@types/googlemaps';
 export class CustomerDashboardComponent implements OnInit {
   @ViewChild('gmapcustomer') gmapElem: any;
   mapObject: google.maps.Map;
+  mapPropObject = {};
+  pos = {};
   infoWindowObject: google.maps.InfoWindow = new google.maps.InfoWindow;
 
   constructor() { }
 
   ngOnInit() {
-    var mapPropObject = {
+    this.mapPropObject = {
      // center: new google.maps.LatLng(43.0417898, -76.1228379),
      center: new google.maps.LatLng(35, -76.1228379),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    this.mapObject = new google.maps.Map(this.gmapElem.nativeElement, mapPropObject);
+    this.mapObject = new google.maps.Map(this.gmapElem.nativeElement, this.mapPropObject);
     console.log(this);
     // this function continuously updates the browser location.
-    updateCoordinates(this);
+    this.updateCoordinates(this);
   }
 
-  function handleLocationError(browserHasGeolocation : boolean, infoWindow : google.maps.InfoWindow,
+  handleLocationError(browserHasGeolocation: boolean, infoWindow: google.maps.InfoWindow,
     pos: google.maps.LatLng) {
       infoWindow.setPosition(pos);
       infoWindow.setContent(browserHasGeolocation ?
                             'Error: The Geolocation service failed.' :
                             'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(this.map);
+      infoWindow.open(this.mapObject);
   }
 
-  var updateCoordinates: any = (thisobject: object) => {
+  updateCoordinates: any = (thisobject: object) => {
     console.log(thisobject);
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition((position)=>{
-        var pos = {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        thisobject.infoWindowObject.setPosition(pos);
-        thisobject.infoWindowObject.setContent('You are here!');
-        thisobject.infoWindowObject.open(thisobject.mapObject);
-        thisobject.mapObject.setCenter(pos);
+        // this.infoWindowObject.setPosition(this.pos);
+        this.infoWindowObject.setContent('You are here!');
+        this.infoWindowObject.open(this.mapObject);
+        // this.mapObject.setCenter(this.pos);
         setTimeout(() => {
-          console.log('executing setTimeout after time limit')
-          updateCoordinates(thisobject);
+          console.log('executing setTimeout after time limit');
+          this.updateCoordinates(thisobject);
         }, 10000);
         console.log('after calling setTimeout');
-      }, ()=>{
-        handleLocationError(true, thisobject.infoWindowObject, thisobject.mapObject.getCenter());
+      }, () => {
+        this.handleLocationError(true, this.infoWindowObject, this.mapObject.getCenter());
       });
-    }else{
-      //browser doesnt support geolocation
-      handleLocationError(false, thisobject.infoWindowObject, thisobject.mapObject.getCenter());
+    } else {
+      // browser doesn't support geolocation
+      this.handleLocationError(false, this.infoWindowObject, this.mapObject.getCenter());
     }
   }
 
